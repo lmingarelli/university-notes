2ae8425e9fcc6b1955e2651e93769b88b971b65d
Lorenzo Mingarelli <lorenzo.mingarelli@arsenalia.com>
2025-12-19 16:30:01 +0100

    2025-07 exam ex1 and ex2

---
+++ algorithm-design/exam-2025-07/ex2.py
@@ -0,0 +1,5 @@
+"""
+Abbiamo una stringa s di lunghezza n ed una stringa s1 di lunghezza m con mn. Vogliamo contare quante volte s1 compare come sottosequenza in s. Ricorda che una sottosequenza di s si ottiene cancellando da s un numero arbitrario di caratteri.
+Ad esempio per s = babgbag e s1 = bag la risposta è 5. Di seguito sono riportate le 5 sottosequenza pari a s1 che è possibile ottenere:
+bag, bag, bag, bag, bag.
+"""
