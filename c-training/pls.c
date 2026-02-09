cd9b6e7c097c9038307aa2c4a032cf0402fae745
Lorenzo Mingarelli <lorenzo.mingarelli00@gmail.com>
2026-02-09 22:57:10 +0100

    pls.c struct WIP

--- c-training/pls.c
+++ c-training/pls.c
@@ -2,27 +2,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+struct pls {
+    uint32_t len;
+    uint32_t refcount;
+    char str[];
+};
 
 char *pls_create(char *init, int len) {
-    char *s = malloc(1+len+1);
-    unsigned char *lenptr = (unsigned char*) s;
-    *lenptr = len;
-    for (int i = 0; i < len; i++) {
-        s[i+1] = init[i];
+    struct pls *p = malloc(sizeof(struct pls) + len + 1);
+    p->len = len;
+    p->refcount = 1;
+
+    for (int i=0; i < len; i++) {
+        p->str[i] = init[i];
     }
-    s[len+1] = 0;
-    return s;
+    p->str[len] = 0;
+    return p->str;
 }
 
+/* Display the string 's' on the screen. */
 void pls_print (char *s) {
-    unsigned char *lenptr = (unsigned char*) s;
-    for (int i = 0; i < *lenptr; i++) {
-        putchar(s[i+1]);
+    struct pls *p = s - sizeof(*p);
+    for (int i=0; i < p->len; i++) {
+        putchar(p->str[i]);
     }
     printf("\n");
+}
+
+void pls_free(char *s) {
+    free(s-sizeof(struct pls));
+}
+
+void pls_release(char *s) {
+    struct pls *p = s - sizeof(*p);
+    if (p->refcount == 0) {
+        printf("ABORTED ON FREE ERROR");
+        exit(1);
+    }
+
+    p->refcount--;
+    if (p->refcount == 0) pls_free(s);
+}
+
+int pls_len(char *s) {
+    struct pls *p = s - sizeof(*p);
+    return p->len;
 }
 
 int main () {
     char *mystr = pls_create("Pippo pluto 123!", 16);
     pls_print(mystr);
+    printf("My string lenght = %d\n", pls_len(mystr));
+    pls_free(mystr);
 }
